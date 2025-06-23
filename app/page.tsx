a9ba632d59b02a87dbf07c3c5fb2ad34d6ade2ff
rajeshrc12 <rajesh.charhajari@gmail.com>
2025-06-23 19:02:31 +0530

    next auth added

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
-export default function Home() {
+import SignIn from "@/components/sign-in";
+import { auth } from "@/auth";
+import SignOut from "@/components/sign-out";
+
+export default async function Home() {
+  const session = await auth();
+  console.log(session);
   return (
     <div>
-      <h1>Home</h1>
+      {session ? <SignOut /> : <SignIn />}
+      <div>{session?.user?.name}</div>
     </div>
   );
 }
